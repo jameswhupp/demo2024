1788fb3e4487cdc43919e50a2a36825246482a0e
jameswhupp <jameswhupp>
2024-08-08 14:16:23 -0400

    intial commit, hopefully excluding all intelliJ-specific files this time, per best practices

---
+++ demo/src/test/java/com/jameswhupp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameswhupp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
