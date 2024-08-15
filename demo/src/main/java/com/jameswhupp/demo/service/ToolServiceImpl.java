38bc23a720bb36098f6be7d821ccd22b517cdfb1
jameswhupp <jameswhupp>
2024-08-14 21:00:04 -0400

    optimized imports

--- demo/src/main/java/com/jameswhupp/demo/service/ToolServiceImpl.java
+++ demo/src/main/java/com/jameswhupp/demo/service/ToolServiceImpl.java
@@ -1,11 +1,11 @@
 package com.jameswhupp.demo.service;
 
-import java.util.Optional;
-
 import com.jameswhupp.demo.data.ToolRepository;
 import com.jameswhupp.demo.model.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ToolServiceImpl implements ToolService {
